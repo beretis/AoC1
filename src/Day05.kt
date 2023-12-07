7b70ffc69058214fe64e55ec9dcc818c74682661
beretis <jojo@matus.sk>
2023-12-08 00:49:22 +0100

    day5_part1_already_fucked_by_part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,21 +1,61 @@
 fun main() {
 
-    fun part1(text: String): Int {
-        text.lines().first().replace("seeds: ", "").split( " ").map { it.toInt() }.println()
-//        text.lines().drop(1).toString().split
-        return 0
+    fun part1(text: String): Long {
+        val seedsRanges = text.lines().first().replace("seeds: ", "").split( " ").map { it.toLong() }
+        val seeds: MutableList<LongRange>  = mutableListOf()
+        for (i in 0 until seedsRanges.size step 2) {
+            // Make sure to not get IndexOutOfBoundsException
+            seeds.add(LongRange(seedsRanges[i], seedsRanges[i] + seedsRanges[i + 1]))
+        }
+
+        val transformations: MutableList<TransformationFunction> = mutableListOf()
+        val splits = text.lines()
+            .drop(2)
+            .filter { it.isNotEmpty() }
+            .map {
+                if (it.contains(':')) {
+                    return@map "*"
+                } else {
+                    return@map it
+                }
+            }
+            .forEach {
+                if (it == "*") {
+                    val transformationFunction = TransformationFunction()
+                    transformations.add(transformationFunction)
+                } else {
+                    val numbers = it.split(" ")
+                    val range = numbers[1].toLong() .. numbers[1].toLong() + numbers[2].toLong()
+                    val offset = -numbers[0].toLong() + numbers[1].toLong()
+                    transformations.last().transformation[range] = offset
+                }
+
+            }
+        var resultArray: MutableList<Long> = mutableListOf()
+        seeds.forEach { range ->
+            range.forEach { seed ->
+                var intermediateSeed = seed
+                transformations.forEach { transformation ->
+                    intermediateSeed = transformation.transform(intermediateSeed)
+                }
+                resultArray.add(intermediateSeed)
+            }
+        }
+
+        return resultArray.min()
     }
 
-    val text = readInputAsText("Day05_test")
+    val text = readInputAsText("Day05")
     val result = part1(text)
+    println("result: $result")
 
 }
 
 
-class TransformationFunction(val id: String) {
-    val transformation: Map<IntRange, Int> = mapOf()
+class TransformationFunction() {
+    val transformation: MutableMap<LongRange, Long> = mutableMapOf()
 
-    fun getRangeFor(input: Int): IntRange? {
+    fun getRangeFor(input: Long): LongRange? {
         transformation.keys.forEach { range ->
             if (input in range) {
                 return range
@@ -24,16 +64,37 @@ class TransformationFunction(val id: String) {
         return null
     }
 
-    fun getOffset(input: Int): Int {
+    fun getOffset(input: Long): Long {
         val range = getRangeFor(input)
         if (range != null) {
             return transformation[range]!!
         }
-        return input
+        return 0
     }
 
-    fun transform(input: Int): Int {
-        return input + getOffset(input)
+    fun transform(input: Long): Long {
+        return input - getOffset(input)
+    }
+
+    fun transformRange(inputRanges: List<LongRange>): List<LongRange> {
+        var splitRanges: MutableList<LongRange> = mutableListOf()
+        return inputRanges.flatMap { transformRange(it) }
+    }
+
+    private fun transformRange(inputRange: LongRange): List<LongRange> {
+        var splitRanges: MutableList<LongRange> = mutableListOf()
+        transformation.forEach {transRange ->
+            var intersect = transRange.key.intersect(inputRange)
+            if (intersect.isNotEmpty()) {
+                val range = transform(intersect.min()) .. transform(intersect.max())
+                splitRanges.add(range)
+            }
+        }
+        return splitRanges
+    }
+
+    override fun toString(): String {
+        return transformation.toString()
     }
 }
 
